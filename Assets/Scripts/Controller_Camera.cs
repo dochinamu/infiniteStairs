162fb8de75ab0ca498478340b55958dcda99d4e2
Kim HyeEun <gpdms6420@naver.com>
2024-04-13 17:59:07 +0900

    Initial commit

---
+++ Assets/Scripts/Controller_Camera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Camera : MonoBehaviour
+{
+
+    public GameObject Target; // 목표 타겟
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            // 카메라가 타겟의 위치로 이동
+            this.transform.position = Target.transform.position + new Vector3(0, 2f, -10f); // this 는 Controller_Camera 스크립트가 붙을 오브젝트
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool Game_Start = false; // 게임 시작 체크
+
+    public float Current_Time = 0.0f; // 현재 남은 시간
+    public float Destination_Time = 10.0f; // 전체 시간(10초)
+    public Slider Slider; // 시간 타이머
+    public float Add_Time_Flow = 0.001f; // 감소 시간
+
+    public Text Text; // 점수 텍스트
+
+    public int Score = 0; // 점수
+
+
+
+    public GameObject Character; // 캐릭터
+    public Transform Platform_Parents; // 정리를 위한 발판들의 부모 오브젝트
+    public GameObject Platform; // 계단
+    private List<GameObject> Platform_List = new List<GameObject>(); // 발판 리스트
+    private List<int> Platform_Check_List = new List<int>(); // 발판의 위치 리스트 (왼쪽: 0, 오른쪽: 1)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Data_Load();
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Game_Start) { // 키보드 입력 체크
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Check_Platform(Character_Pos_Idx, 1);
+            } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Check_Platform(Character_Pos_Idx, 0);
+            }
+            Destination_Time = Destination_Time - Add_Time_Flow;
+            Current_Time = Current_Time - Time.deltaTime; // 프레임 사이 시간이 일정하지 않기 때문에, delta time씩 더해주어 어떤 환경에서든 일정한 시간씩 흐르도록 함
+
+            Slider.value = Current_Time / Destination_Time;
+            
+            if (Current_Time < 0f)
+            {
+                Result();
+            }
+        } else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Init();
+            }
+        }
+    }
+
+    public void Data_Load() // 데이터 로드, 발판 오브젝트 생성
+    {
+        for(int i=0; i < 20; i++)
+        {
+            GameObject t_obj = Instantiate(Platform, Vector3.zero, Quaternion.identity); //복사할 오브젝트, 복제된 오브젝트의 위치, 복제된 프로젝트의 방향(부모 좌표축)
+            t_obj.transform.parent = Platform_Parents;
+            Platform_List.Add(t_obj);
+            Platform_Check_List.Add(0);
+        }
+
+        Platform.SetActive(false);
+    }
+
+
+    private int Pos_Idx = 0; // 발판의 마지막 위치
+    private int Character_Pos_Idx = 0; // 캐릭터 위치
+
+
+    public void Init() // 캐릭터 위치, 발판 위치 초기화
+    {
+        Character.transform.position = Vector3.zero; 
+
+        for (Pos_Idx = 0; Pos_Idx < 20;)
+        {
+            Next_Platform(Pos_Idx);
+        }
+
+        Destination_Time = 10.0f;
+        Current_Time = Destination_Time;
+
+        Character_Pos_Idx = 0;
+        Score = 0;
+        Text.text = Score.ToString();
+
+
+
+        Game_Start = true;
+    }
+
+    public void Next_Platform(int idx)
+    {
+        int pos = Random.Range(0, 2); //0 or 1
+        if (idx == 0) // 첫번째 발판이라면
+        {
+            Platform_List[idx].transform.position = new Vector3(0, -0.5f, 0);
+        }else
+        {
+            if (Pos_Idx < 20)
+            {
+                if (pos == 0) // 왼쪽 발판이라면
+                {
+                    Platform_Check_List[idx - 1] = pos;
+                    Platform_List[idx].transform.position = Platform_List[idx - 1].transform.position + new Vector3(-1f, 0.5f, 0);
+                }
+                else // 오른쪽 발판이라면
+                {
+                    Platform_Check_List[idx - 1] = pos;
+                    Platform_List[idx].transform.position = Platform_List[idx - 1].transform.position + new Vector3(1f, 0.5f, 0);
+                }
+            } else
+            {
+                if (pos == 0) // 왼쪽 발판이라면
+                {
+                    if (idx % 20 == 0)
+                    {
+                        Platform_Check_List[20 - 1] = pos;
+                        Platform_List[idx % 20].transform.position = Platform_List[20 - 1].transform.position + new Vector3(-1f, 0.5f, 0);
+                    } else
+                    {
+                        Platform_Check_List[idx % 20 - 1] = pos;
+                        Platform_List[idx % 20].transform.position = Platform_List[idx % 20 - 1].transform.position + new Vector3(-1f, 0.5f, 0);
+                    }
+                }
+                else // 오른쪽 발판이라면
+                {
+                    if (idx % 20 == 0)
+                    {
+                        Platform_Check_List[20 - 1] = pos;
+                        Platform_List[idx % 20].transform.position = Platform_List[20 - 1].transform.position + new Vector3(1f, 0.5f, 0);
+                    }
+                    else
+                    {
+                        Platform_Check_List[idx % 20 - 1] = pos;
+                        Platform_List[idx % 20].transform.position = Platform_List[idx % 20 - 1].transform.position + new Vector3(1f, 0.5f, 0);
+                    }
+                }
+            }
+            
+        }
+        Score++;
+        Text.text = Score.ToString();
+        Pos_Idx++;
+    }
+
+    void Check_Platform(int idx, int direction)
+    {
+        Debug.Log("idx: " + idx % 20 + " /Platform: " + Platform_Check_List[idx % 20] + " /Direction: " + direction);
+        if (Platform_Check_List[idx % 20] == direction) // 발판이 있음
+        {
+            Character_Pos_Idx++;
+            Character.transform.position = Platform_List[Character_Pos_Idx % 20].transform.position + new Vector3(0f, 0.5f, 0); // 발판 위에 올라가도록 캐릭터 위치 이동
+            Next_Platform(Pos_Idx);
+        } else
+        {
+            Result();
+        }
+    }
+
+    public void Result()
+    {
+        Debug.Log("Game Over");
+        Game_Start = false;
+    }
+}
